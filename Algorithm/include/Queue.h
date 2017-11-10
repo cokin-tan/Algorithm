3c24f047419ed452ac277de5bf90f9c18653efb8
ttan1215225 <316005940@qq.com>
2017-11-10 23:42:05 +0800

    add queue and stack

---
+++ Algorithm/include/Queue.h
@@ -0,0 +1,75 @@
+﻿#pragma once
+
+#include "Utils.h"
+
+template<typename T>
+class CQueue
+{
+public:
+	CQueue(int nSize);
+	virtual ~CQueue();
+
+public:
+	void		EnQueue(T item);
+	bool		IsFull() const;
+	T			DeQueue();
+	bool		IsEmpty() const;
+
+private:
+	int			m_nHead;
+	int			m_nTail;
+	int			m_nLength;
+	T*			m_pArr;
+};
+
+template<typename T>
+CQueue<T>::CQueue(int nSize):
+	m_nLength(nSize + 1),
+	m_nHead(0),
+	m_nTail(0)
+{
+	m_pArr = new T[nSize];
+}
+
+template<typename T>
+CQueue<T>::~CQueue()
+{
+	SAFE_DELETE_ARR(m_pArr);
+}
+
+template<typename T>
+void CQueue<T>::EnQueue(T item)
+{
+	if (IsFull())
+	{
+		throw exception("the queue is full");
+	}
+
+	m_pArr[m_nTail++] = item;
+	m_nTail %= m_nLength;
+}
+
+template<typename T>
+bool CQueue<T>::IsFull() const
+{
+	return (m_nTail + 1) % m_nLength == m_nHead;
+}
+
+template<typename T>
+T CQueue<T>::DeQueue()
+{
+	if (IsEmpty())
+	{
+		throw exception("the queue is empty");
+	}
+
+	T item = m_pArr[m_nHead++];
+	m_nHead %= m_nLength;
+	return item;
+}
+
+template<typename T>
+bool CQueue<T>::IsEmpty() const
+{
+	return m_nHead == m_nTail;
+}
---
+++ Algorithm/include/Stack.h
@@ -0,0 +1,72 @@
+﻿#pragma once
+
+#include <iostream>
+
+#include "Utils.h"
+
+template<typename T>
+class CStack
+{
+public:
+	CStack(int nSize);
+	virtual ~CStack();
+
+public:
+	void		Push(T item);
+	T			Pop();
+	bool		IsEmpty() const;
+	bool		IsFull() const;
+
+private:
+	T*			m_pArr;
+	int			m_nSize;
+	int			m_nTop;
+};
+
+template<typename T>
+CStack<T>::CStack(int nSize):
+	m_nSize(nSize),
+	m_nTop(0)
+{
+	m_pArr = new T[nSize];
+}
+
+template<typename T>
+CStack<T>::~CStack()
+{
+	SAFE_DELETE_ARR(m_pArr);
+}
+
+template<typename T>
+void CStack<T>::Push(T item)
+{
+	if (IsFull())
+	{
+		throw exception("stack is full");
+	}
+
+	m_pArr[m_nTop++] = item;
+}
+
+template<typename T>
+bool CStack<T>::IsFull() const
+{
+	return m_nTop >= m_nSize;
+}
+
+template<typename T>
+T CStack<T>::Pop()
+{
+	if (IsEmpty())
+	{
+		throw exception("stack is empty");
+	}
+
+	return m_pArr[--m_nTop];
+}
+
+template<typename T>
+bool CStack<T>::IsEmpty() const
+{
+	return m_nTop <= 0;
+}
